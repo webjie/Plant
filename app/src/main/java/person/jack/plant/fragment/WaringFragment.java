10f1025f30444f1fdf1d86d9953784ded1cd815c
chenle <chenle@qq.com>
2018-06-04 11:19:51 +0800

    cl waring

---
+++ app/src/main/java/person/jack/plant/fragment/WaringFragment.java
@@ -0,0 +1,25 @@
+package person.jack.plant.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2018/6/4.
+ */
+
+public class WaringFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
