1d5e21a76a2e247c624c65813244ce2a065b94f2
chenle <chenle@qq.com>
2018-06-04 15:39:53 +0800

    cl 百科碎片

---
+++ app/src/main/java/person/jack/plant/fragment/KnowLedgeFragment.java
@@ -0,0 +1,29 @@
+package person.jack.plant.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import person.jack.plant.R;
+
+/**
+ * Created by Administrator on 2018/6/4.
+ */
+
+public class KnowLedgeFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_konwledge,container,false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
