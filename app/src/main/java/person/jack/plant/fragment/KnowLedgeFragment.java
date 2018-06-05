bd322f8b03b8e93854771a0c1d2b17421685d41d
chenle <chenle@qq.com>
2018-06-05 17:21:52 +0800

    cl 百科完成

--- app/src/main/java/person/jack/plant/fragment/KnowLedgeFragment.java
+++ app/src/main/java/person/jack/plant/fragment/KnowLedgeFragment.java
@@ -1,11 +1,17 @@
 package person.jack.plant.fragment;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import person.jack.plant.R;
 
@@ -14,16 +20,31 @@ import person.jack.plant.R;
  */
 
 public class KnowLedgeFragment extends Fragment{
+    private WebView webView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_konwledge,container,false);
+        webView=(WebView)view.findViewById(R.id.web_view);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.hua36.com");
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        webView.destroy();
+        webView=null;
+    }
+
+
 }
