bf0bddaea62d13df8aef242d6880b4f0b7aef2d2
yanxu <1001>
2018-06-13 14:58:07 +0800

    yanxu  增加故障检测功能

---
+++ app/src/main/java/person/jack/plant/activity/FailureWarnActivity.java
@@ -0,0 +1,15 @@
+package person.jack.plant.activity;
+
+
+import android.os.Bundle;
+
+import person.jack.plant.R;
+
+public class FailureWarnActivity extends BaseFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failure_warn);
+    }
+}
